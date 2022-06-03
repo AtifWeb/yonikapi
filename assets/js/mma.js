6503a8a3d110d841bad04c4049fee73e09248c81
Atif Asim <atifasim@Atifs-MacBook-Air.local>
2022-06-03 17:51:51 +0500

    new

--- assets/js/mma.js
+++ assets/js/mma.js
@@ -1,37 +1,65 @@
 import { videos } from "./videos/MMA_videos.js";
-
+import { db } from "../db/config.js";
 let videos_area = document.querySelector(".videos_area");
 
-let VideoTemplate = (list) => {
+let VideoTemplate = (list, newData) => {
   return `
   <a href="./view.html" id="${list.id}" class="image_wrapper">
   <img src=${list.poster} alt="" />
-  <h1>${list.title} <span>(220+ likes this video)</span></h1>
+  <h1>${list.title} <span>(${
+    newData.length > 0 ? newData[0]["data"]["likes"] : 0
+  } likes this video)</span></h1>
 </a>
     `;
 };
 
-videos["list"].forEach((element) => {
-  videos_area.insertAdjacentHTML("beforeend", VideoTemplate(element));
-});
-document
-  .querySelectorAll(".videos_area .image_wrapper")
-  .forEach((EachAnchor) => {
-    EachAnchor.addEventListener("click", (e) => {
-      let id = e.target.id;
-      let data = [];
+const getData = () => {
+  let data = [];
 
-      let filterValue = videos["list"].filter(
-        (EachFilterObj) => EachFilterObj.id == id
-      );
-      console.log(filterValue);
-      if (filterValue.length > 0) {
-        data = filterValue;
-      }
-
-      if (data.length > 0) {
-        let Injson = JSON.stringify(data[0]);
-        window.sessionStorage.setItem("Data", Injson);
-      }
+  db.collection("Articles").onSnapshot((snapshot) => {
+    let changes = snapshot.docChanges();
+    let data = [];
+    changes.forEach((change) => {
+      let idData = change.doc.id;
+      let object = {
+        id: idData,
+        data: change.doc.data(),
+      };
+      data = [...data, object];
     });
+
+    videos["list"].forEach((element) => {
+      let newData = data.filter((EachData) => EachData.id == element.id);
+      console.log(newData);
+      videos_area.insertAdjacentHTML(
+        "beforeend",
+        VideoTemplate(element, newData)
+      );
+    });
+    SetLocalStorageData();
   });
+};
+getData();
+const SetLocalStorageData = (e) => {
+  document
+    .querySelectorAll(".videos_area .image_wrapper")
+    .forEach((EachAnchor) => {
+      EachAnchor.addEventListener("click", (e) => {
+        let id = e.target.id;
+        let data = [];
+
+        let filterValue = videos["list"].filter(
+          (EachFilterObj) => EachFilterObj.id == id
+        );
+        console.log(filterValue);
+        if (filterValue.length > 0) {
+          data = filterValue;
+        }
+
+        if (data.length > 0) {
+          let Injson = JSON.stringify(data[0]);
+          window.sessionStorage.setItem("Data", Injson);
+        }
+      });
+    });
+};
--- assets/js/videos/nfl_videos.js
+++ assets/js/videos/nfl_videos.js
@@ -32,35 +32,6 @@ export const videos = {
       desc: "",
     },
     {
-      id: "NFL1",
-      poster: "https://i.ytimg.com/vi_webp/Sx7SvkKJ9rw/maxresdefault.webp",
-      title: "THIS 6’7” 300-POUND WR BULLIED EVE",
-      videos: ["https://www.youtube.com/embed/yAyuxHAkByw"],
-      desc: "",
-    },
-    {
-      id: "NFL2",
-      poster: "https://i.ytimg.com/vi/xm01LD9A1NE/hqdefault.jpg",
-      title:
-        "Does Jalen Hurts have what it takes to lead the Eagles to a Super Bowl? | NFL Live",
-      videos: ["https://www.youtube.com/embed/8FSrbPXGUpk"],
-      desc: "",
-    },
-    {
-      id: "NFL3",
-      poster: "https://i.ytimg.com/vi_webp/BILXqrR-VAE/maxresdefault.webp",
-      title: "If the NFL was school",
-      videos: ["https://www.youtube.com/embed/9jfJIXBOu2E"],
-      desc: "",
-    },
-    {
-      id: "NFL4",
-      poster: "https://i.ytimg.com/vi_webp/UkCiZ6K30BI/hqdefault.webp",
-      title: "THE FASTEST PLAYER IN THE NFL",
-      videos: ["https://www.youtube.com/embed/8Vk72DzBljw"],
-      desc: "",
-    },
-    {
       id: "NFL5",
       poster: "https://i.ytimg.com/vi_webp/Sx7SvkKJ9rw/maxresdefault.webp",
       title: "THIS 6’7” 300-POUND WR BULLIED EVE",
